4e3ffca1793e32129f4ae911ba54fcf1b96deea6
VoyageLointain <iaixiaoba@163.com>
2024-10-31 22:05:57 +0800

    user thrift

--- rpc/base.thrift
+++ rpc/base.thrift
@@ -1,6 +1,17 @@
-namespace go example.shop.base
+namespace go base
+namespace py base
+namespace java base
+
+struct Base {
+    1: string LogID = "",
+    2: string Caller = "",
+    3: string Addr = "",
+    4: string Client  = "",
+    5: optional map<string,string> Extra,
+}
 
 struct BaseResp {
-    1: string code
-    2: string msg
+    1:string StatusMessage = "",
+    2: i32 StatusCode = 0,
+    3: optional map<string,string> Extra,
 }
